ac1a92281b1a6e8ddf5dd779311321e84131fd49
Casper <caspersmits11@outlook.com>
2017-03-29 14:08:13 +0200

    Fixed seed file

--- database/seed.sql
+++ database/seed.sql
@@ -42,14 +42,14 @@ INSERT INTO `pages` (name_en, name_nl, slug, type) VALUES
   ('Coaching', 'Begeleiding', 'coaching', 'master');
 
 INSERT INTO `content` (program_id, page_id, body) VALUES
-  (@ss_id, (SELECT id FROM pages WHERE slug = 'overview'), 'the overview page'),
-  (@ss_id, (SELECT id FROM pages WHERE slug = 'curriculum'), 'the curriculum page'),
-  (@ss_id, (SELECT id FROM pages WHERE slug = 'learning-objectives'), 'learning objectives page'),
-  (@ss_id, (SELECT id FROM pages WHERE slug = 'professional-skills'), 'the professional skills page'),
-  (@ss_id, (SELECT id FROM pages WHERE slug = 'final-project'), 'final project page'),
-  (@ss_id, (SELECT id FROM pages WHERE slug = 'examination-schedules'), 'examination schedules here'),
-  (@ss_id, (SELECT id FROM pages WHERE slug = 'graduation-dates'), 'graduation dates page'),
-  (@ss_id, (SELECT id FROM pages WHERE slug = 'coaching'), 'coaching page'),
-  (@ss_id, (SELECT id FROM pages WHERE slug = 'regulations'), 'regulations page'),
-  (@ss_id, (SELECT id FROM pages WHERE slug = 'subsequent-masters'), 'subsequent masters page');
+  (@ss_id, (SELECT id FROM pages WHERE slug = 'overview' AND type = 'bachelor' LIMIT 1), 'the overview page'),
+  (@ss_id, (SELECT id FROM pages WHERE slug = 'curriculum' AND type = 'bachelor' LIMIT 1), 'the curriculum page'),
+  (@ss_id, (SELECT id FROM pages WHERE slug = 'learning-objectives' AND type = 'bachelor' LIMIT 1), 'learning objectives page'),
+  (@ss_id, (SELECT id FROM pages WHERE slug = 'professional-skills' AND type = 'bachelor' LIMIT 1), 'the professional skills page'),
+  (@ss_id, (SELECT id FROM pages WHERE slug = 'final-project' AND type = 'bachelor' LIMIT 1), 'final project page'),
+  (@ss_id, (SELECT id FROM pages WHERE slug = 'examination-schedules' AND type = 'bachelor' LIMIT 1), 'examination schedules here'),
+  (@ss_id, (SELECT id FROM pages WHERE slug = 'graduation-dates' AND type = 'bachelor' LIMIT 1), 'graduation dates page'),
+  (@ss_id, (SELECT id FROM pages WHERE slug = 'coaching' AND type = 'bachelor' LIMIT 1), 'coaching page'),
+  (@ss_id, (SELECT id FROM pages WHERE slug = 'regulations' AND type = 'bachelor' LIMIT 1), 'regulations page'),
+  (@ss_id, (SELECT id FROM pages WHERE slug = 'subsequent-masters' AND type = 'bachelor' LIMIT 1), 'subsequent masters page');
 
