03d7dc933ac8b7ccf504209b0be865f62e346141
Maarten Flippo <maartenflippo@outlook.com>
2017-04-02 14:52:23 +0200

    added static localization

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
 $dotenv->load();
 
+$lang = require_once __DIR__ . '/../app/lang/' . strtolower(Language::getLocale()) . '.php';
+
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 $uriParts = explode('/', $uri);
@@ -13,6 +15,14 @@ $pageSlug = $uriParts[count($uriParts) - 1];
 $templates = new \League\Plates\Engine(__DIR__ . '/../app/views');
 $templates->registerFunction('activeLink', function($slug) use ($pageSlug) {
     return ($slug === $pageSlug) ? 'class="active"' : '';
+});
+
+$templates->registerFunction('trans', function($key) use ($lang) {
+    if (array_key_exists($key, $lang)) {
+        return $lang[$key];
+    }
+
+    return $key;
 });
 
 $connection = new PDO('mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
